ad7f476af25a572f196ac52a709af30c6a36790c
jam.chenjun <jam.chenjun@gmail.com>
2017-06-30 21:53:32 +0800

    分类视图支持显示tableView

--- Closet/Home/CJCategoryController.swift
+++ Closet/Home/CJCategoryController.swift
@@ -9,16 +9,22 @@
 
 import UIKit
 private let kPannelViewWidth = 280.0            // 分类面板宽度
+private let kHeaderContentViewHeight = 64.0     // 头部引导视图高度
+private let kCJCategoryTableViewCellReuseIdentifier = "kCJCategoryTableViewCellReuseIdentifier"
 
 protocol CJCategoryControllerDelegate {
     func closePannel() -> Void
 }
 
-class CJCategoryController: UIViewController {
+class CJCategoryController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var pannelView: UIView!
+    var headerContentView: UIView!
+    var tableView: UITableView!
+    
     var closeButton: UIButton!
     var delegate: CJCategoryControllerDelegate?
+    let dataList = ["全部", "外套", "裤子", "袜子", "鞋子"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +39,36 @@ class CJCategoryController: UIViewController {
         self.pannelView.backgroundColor = UIColor.white
         self.pannelView.autoresizingMask = UIViewAutoresizing.flexibleRightMargin
         self.view.addSubview(self.pannelView)
+        
+        // headerContentView
+        self.headerContentView = UIView()
+        frame = self.pannelView.bounds
+        frame.size.height = CGFloat(kHeaderContentViewHeight)
+        self.headerContentView.frame = frame
+        self.headerContentView.backgroundColor = UIColor.lightGray
+        self.pannelView.addSubview(self.headerContentView)
+        
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "分类"
+        titleLabel.textColor = UIColor.white
+        self.headerContentView.addSubview(titleLabel)
+        
+        self.headerContentView.addConstraints([
+            NSLayoutConstraint.init(item: titleLabel, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: titleLabel, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 10)])
+        
+        // tableView
+        self.tableView = UITableView()
+        frame = self.pannelView.bounds
+        frame.origin.y = CGFloat(kHeaderContentViewHeight)
+        frame.size.height -= CGFloat(kHeaderContentViewHeight)
+        self.tableView.frame = frame
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.pannelView.addSubview(self.tableView)
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: kCJCategoryTableViewCellReuseIdentifier)
         
         // closeButton
         self.closeButton = UIButton.init(type: UIButtonType.custom)
@@ -70,7 +106,31 @@ class CJCategoryController: UIViewController {
             self.delegate?.closePannel()
         }
     }
-
+    
+    // UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: kCJCategoryTableViewCellReuseIdentifier, for: indexPath)
+        let row = indexPath.row
+        let title = self.dataList[row]
+        cell.textLabel?.text = title
+        return cell
+    }
+    
+    // UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44.0
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
