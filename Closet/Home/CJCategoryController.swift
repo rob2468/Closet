af8ebf53a4513eb83cf7baf5a8f4d80f6ea5ce31
jam.chenjun <jam.chenjun@gmail.com>
2017-06-30 11:15:22 +0800

    Initial commit

---
+++ Closet/Home/CJCategoryController.swift
@@ -0,0 +1,24 @@
+//
+//  CJCategoryController.swift
+//  Closet
+//
+//  Created by chenjun on 30/06/2017.
+//  Copyright © 2017 chenjun. All rights reserved.
+//
+// 首页分类面板
+
+import UIKit
+
+class CJCategoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Closet/Home/CJExhibitController.swift
@@ -0,0 +1,24 @@
+//
+//  CJExhibitController.swift
+//  Closet
+//
+//  Created by chenjun on 30/06/2017.
+//  Copyright © 2017 chenjun. All rights reserved.
+//
+// 首页展示单品视图
+
+import UIKit
+
+class CJExhibitController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
