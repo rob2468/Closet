443776c4690c190ddebe5a95481db444bf73c4c9
jam.chenjun <jam.chenjun@gmail.com>
2017-08-22 10:31:02 +0800

    分类添加页面支持显示输入框

--- Closet/Category/CJCategoryAddController.swift
+++ Closet/Category/CJCategoryAddController.swift
@@ -17,6 +17,8 @@ protocol CJCategoryAddControllerDelegate {
 class CJCategoryAddController: UIViewController {
 
     var headerContentView: UIView!
+    var categoryNameTextField: UITextField!
+    
     var delegate: CJCategoryAddControllerDelegate?
 
     override func viewDidLoad() {
@@ -31,6 +33,18 @@ class CJCategoryAddController: UIViewController {
         self.headerContentView.backgroundColor = UIColor.lightGray
         self.view.addSubview(self.headerContentView)
         
+        // "新增分类"titleLabel
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = UIColor.white
+        titleLabel.text = "新增分类"
+        titleLabel.font = UIFont.systemFont(ofSize: 18)
+        self.headerContentView.addSubview(titleLabel)
+        
+        self.headerContentView.addConstraints([
+            NSLayoutConstraint.init(item: titleLabel, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: titleLabel, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 10)])
+        
         // "取消“
         let cancelButton = UIButton.init(type: UIButtonType.custom)
         cancelButton.translatesAutoresizingMaskIntoConstraints = false
@@ -43,17 +57,41 @@ class CJCategoryAddController: UIViewController {
             NSLayoutConstraint.init(item: cancelButton, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 17),
             NSLayoutConstraint.init(item: cancelButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 10)])
         
-        // "完成"
+        // "保存"
         let doneButton = UIButton.init(type: UIButtonType.custom)
         doneButton.translatesAutoresizingMaskIntoConstraints = false
         doneButton.titleLabel?.font = UIFont.systemFont(ofSize: 17)
-        doneButton.setTitle("完成", for: UIControlState.normal)
+        doneButton.setTitle("保存", for: UIControlState.normal)
         doneButton.addTarget(self, action: #selector(doneButtonPressed), for: UIControlEvents.touchUpInside)
         self.headerContentView.addSubview(doneButton)
         
         self.headerContentView.addConstraints([
             NSLayoutConstraint.init(item: doneButton, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: -17),
             NSLayoutConstraint.init(item: doneButton, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 10)])
+        
+        // "分类名"
+        let categoryNameLabel = UILabel()
+        categoryNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        categoryNameLabel.text = "分类名："
+        categoryNameLabel.textColor = UIColor.black
+        categoryNameLabel.font = UIFont.systemFont(ofSize: 17)
+        self.view.addSubview(categoryNameLabel)
+        
+        categoryNameLabel.setContentHuggingPriority(UILayoutPriorityDefaultHigh, for: UILayoutConstraintAxis.horizontal)
+        self.view.addConstraints([
+            NSLayoutConstraint.init(item: categoryNameLabel, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.leading, multiplier: 1, constant: 17),
+            NSLayoutConstraint.init(item: categoryNameLabel, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self.headerContentView, attribute: NSLayoutAttribute.bottom, multiplier: 1, constant: 20)])
+        
+        // categoryNameTextField
+        self.categoryNameTextField = UITextField()
+        self.categoryNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        self.categoryNameTextField.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(self.categoryNameTextField)
+        
+        self.view.addConstraints([
+            NSLayoutConstraint.init(item: self.categoryNameTextField, attribute: NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem: categoryNameLabel, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: 0),
+            NSLayoutConstraint.init(item: self.categoryNameTextField, attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal, toItem: self.view, attribute: NSLayoutAttribute.trailing, multiplier: 1, constant: -17),
+            NSLayoutConstraint.init(item: self.categoryNameTextField, attribute: NSLayoutAttribute.centerY, relatedBy: NSLayoutRelation.equal, toItem: categoryNameLabel, attribute: NSLayoutAttribute.centerY, multiplier: 1, constant: 0)])
     }
 
     func cancelButtonPressed() -> Void {
