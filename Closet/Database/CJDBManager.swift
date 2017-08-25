218117a05eebb17519f714e49fb70a4113885a31
jam.chenjun <jam.chenjun@gmail.com>
2017-08-25 17:46:25 +0800

    支持新建“分类”数据库表

--- Closet/Database/CJDBManager.swift
+++ Closet/Database/CJDBManager.swift
@@ -8,21 +8,42 @@
 
 import UIKit
 
+private let kCurrentVersion = 1
+private let kTABLECATEGORY = "category"
+private let kCATEGORYFIELDID = "id"
+private let kCATEGORYFIELDNAME = "name"
+
 class CJDBManager: NSObject {
     static let sharedInstance = CJDBManager()
     var dbFilePath: URL!
-    var db: FMDatabase!
+    var databaseQueue: FMDatabaseQueue!
     
     private override init() {
-        dbFilePath = try! FileManager.default
-            .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            .appendingPathComponent("Closet.sqlite")
-
-        db = FMDatabase(url: dbFilePath)
-        guard db.open() else {
-            print("Unable to open database")
+        super.init()
+        self.dbFilePath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("Closet.sqlite")
+        self.databaseQueue = FMDatabaseQueue.init(url: self.dbFilePath)
+        // 读取数据库版本号
+        var version = 0
+        self.databaseQueue.inDatabase { (db: FMDatabase)->Void in
+            let rs = try? db.executeQuery("PRAGMA user_version", values: nil)
+            if let resultSet = rs {
+                if resultSet.next() {
+                    version = Int(resultSet.int(forColumnIndex: 0))
+                }
+                resultSet.close()
+            }
+        }
+        guard version <= kCurrentVersion else {
             return
         }
+        // 数据库升级
+        self.upgradeDatabaseFromVersion(version)
+        
+//        db = FMDatabase(url: dbFilePath)
+//        guard db.open() else {
+//            print("Unable to open database")
+//            return
+//        }
         
 //        do {
 //            try db.executeUpdate("create table clothes_item (x text, y text, z text)", values: nil)
@@ -40,6 +61,22 @@ class CJDBManager: NSObject {
 //            print("failed: \(error.localizedDescription)")
 //        }
         
-        db.close()
+//        db.close()
+    }
+    
+    func upgradeDatabaseFromVersion(_ version: Int) -> Void {
+        // 数据库逐版本升级
+        for i in version..<kCurrentVersion {
+            if i == 0 {
+                self.databaseQueue.inDatabase({ (db) in
+                    let stat = "CREATE TABLE \(kTABLECATEGORY) (\(kCATEGORYFIELDID) INTEGER PRIMARY KEY AUTOINCREMENT, \(kCATEGORYFIELDNAME) text);"
+                    try? db.executeUpdate(stat, values: nil)
+                })
+            }
+        }
+        // 更新数据库版本号
+        self.databaseQueue.inDatabase { (db) in
+            try? db.executeUpdate("PRAGMA user_version = \(kCurrentVersion);", values: nil)
+        }
     }
 }
